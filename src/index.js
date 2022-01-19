ba690477ad3f463dc294cfbdbabf4636cfeef98b
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-19 11:15:44 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 const userController = require("./Controllers/user.controller")
 const {register, login, verifyToken } = require("./Controllers/auth.controller")
 
-const passport = require("./Configs/passport.google")
+// const passport = require("./Configs/passport.google")
 
 const User = require("./Models/user.model")
 
@@ -53,32 +53,32 @@ app.get("/confrimation/:token", async (req, res) => {
   }
 })
 
-passport.serializeUser(function (user, done) {
-  done(null, user)
-})
+// passport.serializeUser(function (user, done) {
+//   done(null, user)
+// })
 
-passport.deserializeUser(function (user, done) {
-  done(null, user)
-})
+// passport.deserializeUser(function (user, done) {
+//   done(null, user)
+// })
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-)
+// app.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["email", "profile"] })
+// )
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/auth/google/failure",
-  }),
-  (req, res) => {
-    res.status(201).send({ user: req.user?.user, token: req.user?.token })
-  }
-)
+// app.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", {
+//     failureRedirect: "/auth/google/failure",
+//   }),
+//   (req, res) => {
+//     res.status(201).send({ user: req.user?.user, token: req.user?.token })
+//   }
+// )
 
-app.get("/auth/google/failure", (req, res) => {
-  res.send("failure")
-})
+// app.get("/auth/google/failure", (req, res) => {
+//   res.send("failure")
+// })
 
 
 module.exports = app
