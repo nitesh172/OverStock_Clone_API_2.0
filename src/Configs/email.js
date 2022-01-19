880b769b4b2181b15bd16714cb3b290b2d4f35a8
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-19 13:18:13 +0530

    Update

--- src/Configs/email.js
+++ src/Configs/email.js
@@ -1,9 +1,17 @@
 const nodemailer = require("nodemailer")
 
+// module.exports = nodemailer.createTransport({
+//  service: 'gmail',
+//  auth: {
+//         user: 'outstockclone@gmail.com',
+//         pass: 'nitesh12'
+//     }
+// });
 module.exports = nodemailer.createTransport({
- service: 'gmail',
- auth: {
-        user: 'outstockclone@gmail.com',
-        pass: 'nitesh12'
-    }
-});
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "922b8289ff17e5",
+    pass: "708d4aafd8dd98",
+  },
+})
