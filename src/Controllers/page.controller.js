72fbeee2a57915f1a62010c31ef9cc6608226e9b
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-20 00:10:05 +0530

    update

--- src/Controllers/page.controller.js
+++ src/Controllers/page.controller.js
@@ -2,8 +2,18 @@ const {Router} = require("express")
 const crudController = require("./crud.controller")
 const Page = require("../Models/page.model")
 const router = Router()
+const upload = require("./Middlewares/multer")
 
+router.post("", async (req, res) => {
+  try {
+    const page = await Page.create(req.body)
 
+    return res.status(201).send(page)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send(error.message)
+  }
+})
 
 
 module.exports = router
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(cors())
 
 const userController = require("./Controllers/user.controller")
 const {register, login, verifyToken } = require("./Controllers/auth.controller")
+const pageController = require("./Controllers/page.controller")
 const upload = require("./Middlewares/multer")
 
 const passport = require("./Configs/passport.google")
@@ -23,6 +24,7 @@ app.use(express.urlencoded({extended: true}))
 app.use("/users", userController)
 app.post("/register", upload.single("profilePic"), register)
 app.post("/login", login)
+
 app.get("/pages", async (req, res)  => {
   try {
     return res.status(200).render("pages.ejs")
@@ -31,6 +33,9 @@ app.get("/pages", async (req, res)  => {
     res.status(500).send(error.message)
   }
 })
+
+app.use("/pages/upload", pageController)
+
 app.get("/confrimation/:token", async (req, res) => {
   try {
     const user = await verifyToken(req.params.token)
