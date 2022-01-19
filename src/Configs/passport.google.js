8c7fa4f8b9a19d93bb616186c123d9f5d5108270
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-19 11:55:11 +0530

    Update

--- src/Configs/passport.google.js
+++ src/Configs/passport.google.js
@@ -2,7 +2,7 @@ const GoogleStrategy = require("passport-google-oauth2").Strategy
 const passport = require("passport")
 // require("dotenv").config()
 const { newToken } = require("../Controllers/auth.controller")
-// const FacebookStrategy = require("passport-facebook").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const User = require("../Models/user.model")
 const { v4: uuidv4 } = require("uuid")
 
