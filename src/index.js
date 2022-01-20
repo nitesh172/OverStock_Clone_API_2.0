4e931e73fddb0fee007644ec11bf587f74b3aef3
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-20 18:58:21 +0530

    update

--- src/index.js
+++ src/index.js
@@ -31,122 +31,122 @@ app.post("/pages/create", fieldWise(), async (req, res) => {
     const category = [
       {
         name: req.body.c1,
-        imgUrl: imgFolder.c1img.location,
+        imgUrl: imgFolder.c1img[0].location,
       },
       {
         name: req.body.c2,
-        imgUrl: imgFolder.c2img.location,
+        imgUrl: imgFolder.c2img[0].location,
       },
       {
         name: req.body.c3,
-        imgUrl: imgFolder.c3img.location,
+        imgUrl: imgFolder.c3img[0].location,
       },
       {
         name: req.body.c4,
-        imgUrl: imgFolder.c4img.location,
+        imgUrl: imgFolder.c4img[0].location,
       },
       {
         name: req.body.c5,
-        imgUrl: imgFolder.c5img.location,
+        imgUrl: imgFolder.c5img[0].location,
       },
       {
         name: req.body.c6,
-        imgUrl: imgFolder.c6img.location,
+        imgUrl: imgFolder.c6img[0].location,
       },
       {
         name: req.body.c7,
-        imgUrl: imgFolder.c7img.location,
+        imgUrl: imgFolder.c7img[0].location,
       },
       {
         name: req.body.c8,
-        imgUrl: imgFolder.c8img.location,
+        imgUrl: imgFolder.c8img[0].location,
       },
       {
         name: req.body.c9,
-        imgUrl: imgFolder.c9img.location,
+        imgUrl: imgFolder.c9img[0].location,
       },
       {
         name: req.body.c10,
-        imgUrl: imgFolder.c10img.location,
+        imgUrl: imgFolder.c10img[0].location,
       },
       {
         name: req.body.c11,
-        imgUrl: imgFolder.c11img.location,
+        imgUrl: imgFolder.c11img[0].location,
       },
       {
         name: req.body.c12,
-        imgUrl: imgFolder.c12img.location,
+        imgUrl: imgFolder.c12img[0].location,
       },
     ]
 
     const moreCategory = [
       {
         name: req.body.c1n,
-        imgUrl: imgFolder.c1nimg.location,
+        imgUrl: imgFolder.c1nimg[0].location,
       },
       {
         name: req.body.c2n,
-        imgUrl: imgFolder.c2nimg.location,
+        imgUrl: imgFolder.c2nimg[0].location,
       },
       {
         name: req.body.c3n,
-        imgUrl: imgFolder.c3nimg.location,
+        imgUrl: imgFolder.c3nimg[0].location,
       },
       {
         name: req.body.c4n,
-        imgUrl: imgFolder.c4nimg.location,
+        imgUrl: imgFolder.c4nimg[0].location,
       },
       {
         name: req.body.c5n,
-        imgUrl: imgFolder.c5nimg.location,
+        imgUrl: imgFolder.c5nimg[0].location,
       },
       {
         name: req.body.c6n,
-        imgUrl: imgFolder.c6nimg.location,
+        imgUrl: imgFolder.c6nimg[0].location,
       },
       {
         name: req.body.c7n,
-        imgUrl: imgFolder.c7nimg.location,
+        imgUrl: imgFolder.c7nimg[0].location,
       },
       {
         name: req.body.c8n,
-        imgUrl: imgFolder.c8nimg.location,
+        imgUrl: imgFolder.c8nimg[0].location,
       },
       {
         name: req.body.c9n,
-        imgUrl: imgFolder.c9nimg.location,
+        imgUrl: imgFolder.c9nimg[0].location,
       },
       {
         name: req.body.c10n,
-        imgUrl: imgFolder.c10nimg.location,
+        imgUrl: imgFolder.c10nimg[0].location,
       },
       {
         name: req.body.c11n,
-        imgUrl: imgFolder.c11nimg.location,
+        imgUrl: imgFolder.c11nimg[0].location,
       },
       {
         name: req.body.c12n,
-        imgUrl: imgFolder.c12nimg.location,
+        imgUrl: imgFolder.c12nimg[0].location,
       },
     ]
 
     const page = await Page.create({
       pageName: req.body.pageName,
-      imgUrl: imgFolder.imgUrl.location,
-      img1: imgFolder.img1.location,
-      img2: imgFolder.img2.location,
+      imgUrl: imgFolder.imgUrl[0].location,
+      img1: imgFolder.img1[0].location,
+      img2: imgFolder.img2[0].location,
       text1: req.body.text1,
       text2: req.body.text2,
       text3: req.body.text3,
       text4: req.body.text4,
       category: category,
       imb1: {
-        imgUrl: imgFolder.imbImg1.location,
+        imgUrl: imgFolder.imbImg1[0].location,
         title: req.body.imgbt1,
         desc: req.body.desc1,
       },
       imb2: {
-        imgUrl: imgFolder.imbImg2.location,
+        imgUrl: imgFolder.imbImg2[0].location,
         title: req.body.imgbt2,
         desc: req.body.desc2,
       },
