fdc7405843462ba377b4bc32cdd9c57b914f9f8e
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-20 15:59:40 +0530

    update

--- src/index.js
+++ src/index.js
@@ -122,7 +122,7 @@ app.get(
 
 app.post("/upload", uploadUsers("uploadPic"), (req, res) => {
   try {
-    const data = JSON.parse(req.files)
+    const data = JSON.stringify(req.files)
     res.render("data.ejs",{data: data})
   } catch (error) {
     res.send(error.message)
