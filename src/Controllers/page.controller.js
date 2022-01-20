14b3b89ba2e096310c9b5c9cec888b7f647ed9af
Rahul Kumar <rahulkumar90270@gmail.com>
2022-01-20 23:42:21 +0530

    code simiplification

--- src/Controllers/page.controller.js
+++ src/Controllers/page.controller.js
@@ -4,110 +4,106 @@ const Page = require("../Models/page.model")
 const router = Router()
 const { fieldWise } = require("../Middlewares/multer")
 
+function CreateObject(name,imgUrl){
+    return {
+      name,
+      imgUrl
+    }
+}
+
 router.post("/create", fieldWise(), async (req, res) => {
   try {
     const imgFolder = req.files
-    const category = [
-      {
-        name: req.body.c1,
-        imgUrl: imgFolder.c1img[0].location,
-      },
-      {
-        name: req.body.c2,
-        imgUrl: imgFolder.c2img[0].location,
-      },
-      {
-        name: req.body.c3,
-        imgUrl: imgFolder.c3img[0].location,
-      },
-      {
-        name: req.body.c4,
-        imgUrl: imgFolder.c4img[0].location,
-      },
-      {
-        name: req.body.c5,
-        imgUrl: imgFolder.c5img[0].location,
-      },
-      {
-        name: req.body.c6,
-        imgUrl: imgFolder.c6img[0].location,
-      },
-      {
-        name: req.body.c7,
-        imgUrl: imgFolder.c7img[0].location,
-      },
-      {
-        name: req.body.c8,
-        imgUrl: imgFolder.c8img[0].location,
-      },
-      {
-        name: req.body.c9,
-        imgUrl: imgFolder.c9img[0].location,
-      },
-      {
-        name: req.body.c10,
-        imgUrl: imgFolder.c10img[0].location,
-      },
-      {
-        name: req.body.c11,
-        imgUrl: imgFolder.c11img[0].location,
-      },
-      {
-        name: req.body.c12,
-        imgUrl: imgFolder.c12img[0].location,
-      },
-    ]
+    let category = [];
+    let moreCategory = [];
+    for(let i=1; i<=12; i++){
+      let a = "req.body.c" + i;
+      let b = "imgFolder.c" + i + "img[0].location";
+      let c = "req.body.c" + i + "n";
+      let d = "imgFolder.c" + i + "nimg[0].location";
+      a = eval(a);
+      b = eval(b);
+      c = eval(c);
+      d = eval(d);
+      category.push(CreateObject(a,b));
+      moreCategory.push(CreateObject(c,d));
+    }
+    // const category = [
+    //   CreateObject(req.body.c1,imgFolder.c1img[0].location),
+    //   CreateObject(req.body.c2,imgFolder.c2img[0].location),
+    //   CreateObject(req.body.c3,imgFolder.c3img[0].location),
+    //   CreateObject(req.body.c4,imgFolder.c4img[0].location),
+    //   CreateObject(req.body.c5,imgFolder.c5img[0].location),
+    //   CreateObject(req.body.c6,imgFolder.c6img[0].location),
+    //   CreateObject(req.body.c7,imgFolder.c7img[0].location),
+    //   CreateObject(req.body.c8,imgFolder.c8img[0].location),
+    //   CreateObject(req.body.c9,imgFolder.c9img[0].location),
+    //   CreateObject(req.body.c10,imgFolder.c10img[0].location),
+    //   CreateObject(req.body.c11,imgFolder.c11img[0].location),
+    //   CreateObject(req.body.c12,imgFolder.c12img[0].location)
+    // ]
 
-    const moreCategory = [
-      {
-        name: req.body.c1n,
-        imgUrl: imgFolder.c1nimg[0].location,
-      },
-      {
-        name: req.body.c2n,
-        imgUrl: imgFolder.c2nimg[0].location,
-      },
-      {
-        name: req.body.c3n,
-        imgUrl: imgFolder.c3nimg[0].location,
-      },
-      {
-        name: req.body.c4n,
-        imgUrl: imgFolder.c4nimg[0].location,
-      },
-      {
-        name: req.body.c5n,
-        imgUrl: imgFolder.c5nimg[0].location,
-      },
-      {
-        name: req.body.c6n,
-        imgUrl: imgFolder.c6nimg[0].location,
-      },
-      {
-        name: req.body.c7n,
-        imgUrl: imgFolder.c7nimg[0].location,
-      },
-      {
-        name: req.body.c8n,
-        imgUrl: imgFolder.c8nimg[0].location,
-      },
-      {
-        name: req.body.c9n,
-        imgUrl: imgFolder.c9nimg[0].location,
-      },
-      {
-        name: req.body.c10n,
-        imgUrl: imgFolder.c10nimg[0].location,
-      },
-      {
-        name: req.body.c11n,
-        imgUrl: imgFolder.c11nimg[0].location,
-      },
-      {
-        name: req.body.c12n,
-        imgUrl: imgFolder.c12nimg[0].location,
-      },
-    ]
+    // const moreCategory = [
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   CreateObject(req.body.c2n,imgFolder.c2nimg[0].location),
+    //   {
+    //     name: req.body.c2n,
+    //     imgUrl: imgFolder.c2nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c3n,
+    //     imgUrl: imgFolder.c3nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c4n,
+    //     imgUrl: imgFolder.c4nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c5n,
+    //     imgUrl: imgFolder.c5nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c6n,
+    //     imgUrl: imgFolder.c6nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c7n,
+    //     imgUrl: imgFolder.c7nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c8n,
+    //     imgUrl: imgFolder.c8nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c9n,
+    //     imgUrl: imgFolder.c9nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c10n,
+    //     imgUrl: imgFolder.c10nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c11n,
+    //     imgUrl: imgFolder.c11nimg[0].location,
+    //   },
+    //   {
+    //     name: req.body.c12n,
+    //     imgUrl: imgFolder.c12nimg[0].location,
+    //   },
+    // ]
     const page = await Page.create({
       pageName: req.body.pageName,
       imgUrl: imgFolder.imgUrl[0].location,
