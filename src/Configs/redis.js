7966bbbcd8d833e00f5464f19f5f36f826ddc945
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-21 16:38:16 +0530

    updated

--- src/Configs/redis.js
+++ src/Configs/redis.js
@@ -1,6 +1,8 @@
 const redis = require("redis")
 
-const client = redis.createClient()
+const client = redis.createClient({
+  url: "redis://:p0d30b28d691d0e6b5ec4becce262f950128f5a9317cda76f673b3331c858ac7d@ec2-3-213-154-214.compute-1.amazonaws.com:18509",
+})
 
 client.on("connect", (err) => {
   if (err) console.log(err.message)
