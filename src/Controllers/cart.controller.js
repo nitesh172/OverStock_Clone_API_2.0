4bde55fd4d54fb7721b561e009c6fa8b3f820481
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-23 10:51:21 +0530

    cart

--- src/Controllers/cart.controller.js
+++ src/Controllers/cart.controller.js
@@ -4,8 +4,8 @@ const router = Router()
 
 router.post("", (req, res) => {
   try {
-      console.log(req.body, "hello rahul")
-    redis.set(req.body.email, JSON.stringify(req.body.data))
+      console.log("hello rahul", req.body)
+    redis.set(req.body.email, req.body.data)
     return res.status(201).send({ message: "data updated" })
   } catch (error) {
     console.log(error.message)
@@ -17,7 +17,7 @@ router.get("", (req, res) => {
   try {
     redis.get(req.body.email, async (err, value) => {
       if (err) console.log(err.message)
-      value = JSON.parse(value)
+    //   value = JSON.parse(value)
       return res.status(200).send(value)
     })
   } catch (error) {
