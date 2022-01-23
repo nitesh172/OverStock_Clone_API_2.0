591b328bdc2bf251afc5d5ee0ee4cd9030094122
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-01-23 11:01:39 +0530

    cart

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-// const cors = require("cors")
+const cors = require("cors")
 
-// app.use(cors())
+app.use(cors())
 
 const userController = require("./Controllers/user.controller")
 const pageController = require("./Controllers/page.controller")
